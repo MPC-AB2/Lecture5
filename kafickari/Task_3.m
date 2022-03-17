8bf09000401492cad9aed5cb61538e472e631caf
ASchneiderova <asackob@gmail.com>
2022-03-17 16:59:14 +0100

    last push
    Signed-off-by: ASchneiderova <asackob@gmail.com>

--- kafickari/Task_3.m
+++ kafickari/Task_3.m
@@ -4,17 +4,14 @@ clear all
 close all
 clc
 tempFile = 'TempFile';
-addpath('C:\Users\xschne08\Documents\mpc_ab2\Lecture5')
 
+addpath('C:\Users\xpirkl04\Documents\mpc_ab2\Lecture5')
 
-% try
-%     rmdir(tempFile);
-% end
+try
+    rmdir(tempFile);
+end
 
-
-%%
-
-
+mkdir(tempFile);
 
 load data3.mat
 
@@ -31,72 +28,37 @@ title('Moving')
 
 [~,elastix_version] = system('elastix\elastix.exe --version')
 
-%% mask
-BW = zeros(size(fixed));
-BW(fixed<80) = 1;
-BW = imfill(BW);
-imshow(BW,[])
-pom = zeros(size(BW));
-CC = bwconncomp(BW);
-numPixels = cellfun(@numel,CC.PixelIdxList);
-[biggest,idx] = max(numPixels);
-BW(CC.PixelIdxList{idx}) = 0;
-CC = bwconncomp(BW);
-numPixels = cellfun(@numel,CC.PixelIdxList);
-[biggest,idx] = max(numPixels);
-BW(CC.PixelIdxList{idx}) = 0;
-CC = bwconncomp(BW);
-numPixels = cellfun(@numel,CC.PixelIdxList);
-[biggest,idx] = max(numPixels);
-pom(CC.PixelIdxList{idx}) = 1;
-BW(CC.PixelIdxList{idx}) = 0;
-CC = bwconncomp(BW);
-numPixels = cellfun(@numel,CC.PixelIdxList);
-[biggest,idx] = max(numPixels);
-pom(CC.PixelIdxList{idx}) = 1;
-imshow(pom)
-
-mask_LUNG = pom;
-
 %% Registratio - RIGID AND AFFINE
 
 % save mat to raw - moving and fixed
-mat2raw(fixed,tempFile,'fixed')
-mat2raw(moving,tempFile,'moving')
+mat2raw(fixed,tempFile,'fixed');
+mat2raw(moving,tempFile,'moving');
 
-% create mask
+%create masks
 movingMask = moving>0;
-fixwedMask = fixed>0;
-
-mat2rawMASK(mask_LUNG,tempFile,'fixedM')
-mat2rawMASK(movingMask,tempFile,'movingM')
-
-
-NewPath = [pwd filesep tempFile];
-ParPath = [pwd filesep 'parameter_files_kafickari']
-
+fixedMask = fixed>0;
+mat2rawMASK(fixedMask,tempFile,'fixedM');
+mat2rawMASK(movingMask,tempFile,'movingM');
 
 % run ELASTIX
-CMD = ['elastix\elastix -f ' NewPath '\fixed.mhd -m ' NewPath...
-    '\moving.mhd -out ' NewPath ' -p '  ParPath '\Parameters_BSpline.txt -fMask ' NewPath '\fixedM.mhd -mMask ' NewPath '\movingM.mhd']; %elastix
 
-%% from zoo
-% CMD = ['elastix\elastix -f ' NewPath '\fixed.mhd -m ' NewPath '\moving.mhd -fMask ' NewPath '\fixedM.mhd -out ' NewPath ' -p ' ParPath '\params.txt']; %elastix
+NewPath = [pwd filesep tempFile];
+ParPath = [pwd filesep 'parameter_files_kafickari'];
 
-% 
+%elastix
+CMD = ['elastix\elastix -f ' NewPath '\fixed.mhd -m ' NewPath '\moving.mhd -out ' NewPath ' -p ' ParPath '\Parameters_BSpline.txt'];
+
+%transformix
 % CMD = ['elastix\transformix -def all -in ' NewPath '\moving.mhd -out ' NewPath ' -tp ' NewPath '\TransformParameters.0.txt'];
-status = system(CMD)
 
+status = system(CMD);
 
 
 % read resulting RAW a save to variable - registered
-
-registered = raw2mat([NewPath filesep 'result.0.mhd']);
-
-
+registered = raw2mat([NewPath '\result.0.mhd']);
 
 registered(registered>(35000))=0;
-
+registered_u8 = uint8(im2double(registered).*(2^8));
 %% display
 subplot(1,3,3)
 imshow(registered,[])
@@ -107,6 +69,5 @@ subplot(1,2,1)
 imshowpair(fixed,moving)
 subplot(1,2,2)
 imshowpair(fixed,registered)
-
-%% evalution
-registered1 = uint8(im2double(registered).*(2^8));
+%%
+[evalLung, evalOther] = Eval_Lung2D(registered_u8)
